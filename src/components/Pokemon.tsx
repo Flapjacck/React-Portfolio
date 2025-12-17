56aed3f513d91b1a7988da98f3f542cfd6337165
Flapjacck <plkelly333@me.com>
2025-12-17 14:13:19 -0500

    Fix: pkmn fetch to webp

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -2,9 +2,9 @@ export const Pokemon: React.FC = () => {
   return (
     <div className="fixed bottom-0 right-0 m-4 hidden md:block">
       <img
-        src="https://projectpokemon.org/images/shiny-sprite/lugia.gif"
-        alt="Lugia"
-        className="w-24 h-24"
+        src="/Shiny_yveltal.webp"
+        alt="Yveltal"
+        className="w-27 h-27"
       />
     </div>
   );
